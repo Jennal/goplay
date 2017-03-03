50d64eaa3c6cf3b429c5c10f42487e33b98e3876
jennal <jennalcn@gmail.com>
2017-03-03 16:24:36 +0800

    add data set to session

--- session/session.go
+++ session/session.go
@@ -2,5 +2,208 @@ package session
 
 type Session struct {
 	ID   int
-	Data map[string]interface{}
+	data map[string]interface{}
+}
+
+func NewSession(id int) *Session {
+	return &Session{
+		ID:   id,
+		data: make(map[string]interface{}),
+	}
+}
+
+func (s *Session) Remove(key string) {
+	delete(s.data, key)
+}
+
+func (s *Session) Set(key string, value interface{}) {
+	s.data[key] = value
+}
+
+func (s *Session) HasKey(key string) bool {
+	_, has := s.data[key]
+	return has
+}
+
+func (s *Session) Int(key string) int {
+	v, ok := s.data[key]
+	if !ok {
+		return 0
+	}
+
+	value, ok := v.(int)
+	if !ok {
+		return 0
+	}
+	return value
+}
+
+func (s *Session) Int8(key string) int8 {
+	v, ok := s.data[key]
+	if !ok {
+		return 0
+	}
+
+	value, ok := v.(int8)
+	if !ok {
+		return 0
+	}
+	return value
+}
+
+func (s *Session) Int16(key string) int16 {
+	v, ok := s.data[key]
+	if !ok {
+		return 0
+	}
+
+	value, ok := v.(int16)
+	if !ok {
+		return 0
+	}
+	return value
+}
+
+func (s *Session) Int32(key string) int32 {
+	v, ok := s.data[key]
+	if !ok {
+		return 0
+	}
+
+	value, ok := v.(int32)
+	if !ok {
+		return 0
+	}
+	return value
+}
+
+func (s *Session) Int64(key string) int64 {
+	v, ok := s.data[key]
+	if !ok {
+		return 0
+	}
+
+	value, ok := v.(int64)
+	if !ok {
+		return 0
+	}
+	return value
+}
+
+func (s *Session) Uint(key string) uint {
+	v, ok := s.data[key]
+	if !ok {
+		return 0
+	}
+
+	value, ok := v.(uint)
+	if !ok {
+		return 0
+	}
+	return value
+}
+
+func (s *Session) Uint8(key string) uint8 {
+	v, ok := s.data[key]
+	if !ok {
+		return 0
+	}
+
+	value, ok := v.(uint8)
+	if !ok {
+		return 0
+	}
+	return value
+}
+
+func (s *Session) Uint16(key string) uint16 {
+	v, ok := s.data[key]
+	if !ok {
+		return 0
+	}
+
+	value, ok := v.(uint16)
+	if !ok {
+		return 0
+	}
+	return value
+}
+
+func (s *Session) Uint32(key string) uint32 {
+	v, ok := s.data[key]
+	if !ok {
+		return 0
+	}
+
+	value, ok := v.(uint32)
+	if !ok {
+		return 0
+	}
+	return value
+}
+
+func (s *Session) Uint64(key string) uint64 {
+	v, ok := s.data[key]
+	if !ok {
+		return 0
+	}
+
+	value, ok := v.(uint64)
+	if !ok {
+		return 0
+	}
+	return value
+}
+
+func (s *Session) Float32(key string) float32 {
+	v, ok := s.data[key]
+	if !ok {
+		return 0
+	}
+
+	value, ok := v.(float32)
+	if !ok {
+		return 0
+	}
+	return value
+}
+
+func (s *Session) Float64(key string) float64 {
+	v, ok := s.data[key]
+	if !ok {
+		return 0
+	}
+
+	value, ok := v.(float64)
+	if !ok {
+		return 0
+	}
+	return value
+}
+
+func (s *Session) String(key string) string {
+	v, ok := s.data[key]
+	if !ok {
+		return ""
+	}
+
+	value, ok := v.(string)
+	if !ok {
+		return ""
+	}
+	return value
+}
+
+func (s *Session) Value(key string) interface{} {
+	return s.data[key]
+}
+
+// Retrieve all session state
+func (s *Session) State() map[string]interface{} {
+	return s.data
+}
+
+// Restore session state after reconnect
+func (s *Session) Restore(data map[string]interface{}) {
+	s.data = data
 }
