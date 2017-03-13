c2f85ef05e193fd7ef0034d19c3f7cee5eeb83b3
jennal <jennalcn@gmail.com>
2017-03-13 14:47:57 +0800

    add license comments

--- main.go
+++ main.go
@@ -1,3 +1,15 @@
+// Copyright (C) 2017 Jennal(jennalcn@gmail.com). All rights reserved.
+//
+// Licensed under the MIT License (the "License"); you may not use this file except
+// in compliance with the License. You may obtain a copy of the License at
+//
+// http://opensource.org/licenses/MIT
+//
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
 package main
 
 import (
@@ -57,7 +69,7 @@ func main() {
 	fmt.Scanf("%s", nil)
 
 	cli := tcp.NewClient()
-	client := service.NewServiceClient(cli, pkg.ENCODING_JSON)
+	client := service.NewServiceClient(cli)
 	client.AddListener("test.push", func(line string) {
 		fmt.Println("[test.push] recv: ", line)
 	})
