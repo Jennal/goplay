af2191486681ec25b623658a6fb709bde805c7b6
Jennal <jennalcn@gmail.com>
2017-03-16 16:24:27 +0800

    add comments

--- filter/heartbeat/heartbeat_manager.go
+++ filter/heartbeat/heartbeat_manager.go
@@ -10,6 +10,7 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
+//Package heartbeat resolve half connect issue
 package heartbeat
 
 import (
