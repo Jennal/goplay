9bd5c98a438fcda2b996bb1661ce4a3f877addcc
Jennal <jennalcn@gmail.com>
2017-03-16 15:39:30 +0800

    add comments

--- encode/helper.go
+++ encode/helper.go
@@ -5,9 +5,9 @@
 //
 // http://opensource.org/licenses/MIT
 //
-// Unless required by applicable law or agreed to in writing, software distributed 
-// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
-// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
 package encode
@@ -18,22 +18,25 @@ import (
 	"github.com/jennal/goplay/pkg"
 )
 
-var encoder_map map[pkg.EncodingType]EncodeDecoder = map[pkg.EncodingType]EncodeDecoder{
+var encoderMap = map[pkg.EncodingType]EncodeDecoder{
 	pkg.ENCODING_GOB:  Gob{},
 	pkg.ENCODING_JSON: Json{},
 	pkg.ENCODING_BSON: Bson{},
 }
 
+//GetEncodeDecoder gets EncodeDecoder by pkg.EncodingType
 func GetEncodeDecoder(encoding pkg.EncodingType) EncodeDecoder {
-	return encoder_map[encoding]
+	return encoderMap[encoding]
 }
 
+//UnMarshalHeader decode header from binary data
 func UnMarshalHeader(data []byte) (*pkg.Header, int, error) {
 	header := &pkg.Header{}
 	n, err := pkg.UnmarshalHeader(data, header)
 	return header, n, err
 }
 
+//Marshal encode header and content into binary data
 func Marshal(header *pkg.Header, content interface{}) ([]byte, error) {
 	encoder := GetEncodeDecoder(header.Encoding)
 	contentBuff, err := encoder.Marshal(content)
@@ -54,6 +57,7 @@ func Marshal(header *pkg.Header, content interface{}) ([]byte, error) {
 	return buffer.Bytes(), nil
 }
 
+//Unmarshal decode header and content from binary data
 func Unmarshal(data []byte, header *pkg.Header, content interface{}) error {
 	n, err := pkg.UnmarshalHeader(data, header)
 	if err != nil {
--- encode/json.go
+++ encode/json.go
@@ -5,15 +5,17 @@
 //
 // http://opensource.org/licenses/MIT
 //
-// Unless required by applicable law or agreed to in writing, software distributed 
-// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
-// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
+//Package encode is Encoder and Decoder for pkg
 package encode
 
 import "encoding/json"
 
+//Json encoder/decoder
 type Json struct {
 }
 
