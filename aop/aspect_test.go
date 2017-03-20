d6c50e8271a847f91b4d114a332d0f7d1d76f4cc
jennal <jennalcn@gmail.com>
2017-03-20 11:19:23 +0800

    aop test

--- aop/aspect_test.go
+++ aop/aspect_test.go
@@ -5,9 +5,9 @@
 //
 // http://opensource.org/licenses/MIT
 //
-// Unless required by applicable law or agreed to in writing, software distributed 
-// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
-// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
 package aop
@@ -24,6 +24,48 @@ func TestAop(t *testing.T) {
 		Delay(10 * time.Second).
 		Repeat(5).
 		Do(func() {
-		fmt.Println("Test")
+			fmt.Println("Test")
+		})
+}
+
+func TestSequence(t *testing.T) {
+	Sequence(func(next chan bool, exit chan bool) int {
+		t.Log(1, "=>", 1)
+		next <- true
+		return 1
+	}, func(next chan bool, exit chan bool, a int) int {
+		t.Log(2, "=>", a)
+		next <- true
+		return a + 1
+	}, func(next chan bool, exit chan bool, a int) int {
+		t.Log(3, "=>", a)
+		next <- true
+		return a + 1
+	}, func(next chan bool, exit chan bool, a int) int {
+		t.Log(4, "=>", a)
+		exit <- true
+		return a + 1
+	}, func(next chan bool, exit chan bool, a int) {
+		t.Log(5, "=>", a)
+		next <- true
+	})
+}
+
+func TestParallel(t *testing.T) {
+	Parallel(func(complete chan bool) {
+		t.Log(1)
+		complete <- true
+	}, func(complete chan bool) {
+		t.Log(2)
+		complete <- true
+	}, func(complete chan bool) {
+		t.Log(3)
+		complete <- true
+	}, func(complete chan bool) {
+		t.Log(4)
+		complete <- true
+	}, func(complete chan bool) {
+		t.Log(5)
+		complete <- true
 	})
 }
